fbf0ce4e792583eaf7d66b2556829df80d4ac714
James Allbee <95730407+bytepedaler@users.noreply.github.com>
2024-06-04 19:17:53 -0700

    Added multiple additional input steps, separated into individual functions, BUG: now printing 3x prompts (???)
    Signed-off-by: bisaccharides <95730407+bisaccharides@users.noreply.github.com>

--- main.py
+++ main.py
@@ -8,58 +8,82 @@ back to the user and matches them to one of the studies
 OR states why they met exclusion criteria.
 """
 
-# Take refraction as input
-sphere_power = float(input("Enter the sphere power (include negative sign, if applicable): "))
-cylinder_power = float(input("Enter the cylinder power (include negative sign, if applicable): "))
-axis_degrees = int(input("Enter the axis in degrees: "))
+def intro():
+    print("Welcome to the GMOPC Research Study Matcher.")
+    print("I'm going to ask a series of questions about patient data.")
+    print("Some of the answers may seem obvious, but it's simply in the sake of clarity.")
+    print("Let's begin: ")
 
-# If input is in plus-cylinder, convert to minus-cylinder.
-# Else, assign the negative cylinder powers to the correct variable.
-if cylinder_power >= 0.00:
-    sph_pow_neg_cyl = sphere_power + cylinder_power
-    neg_cyl_pow = 0.00 - cylinder_power
-    # Convert axis degrees
-    if axis_degrees >= 90:
-        neg_cyl_axis_deg = axis_degrees - 90
+def acqr_rfrx():
+    # Take refraction as input
+    sphere_power = float(input("Enter the sphere power (include negative sign, if applicable): "))
+    cylinder_power = float(input("Enter the cylinder power (include negative sign, if applicable): "))
+    axis_degrees = int(input("Enter the axis in degrees: "))
+    return sphere_power, cylinder_power, axis_degrees
+
+def cyl_conv():
+    sphere_power, cylinder_power, axis_degrees = acqr_rfrx()
+    # If input is in plus-cylinder, convert to minus-cylinder.
+    # Else, assign the negative cylinder powers to the correct variable.
+    if cylinder_power >= 0.00:
+        sph_pow_neg_cyl = sphere_power + cylinder_power
+        neg_cyl_pow = 0.00 - cylinder_power
+        # Convert axis degrees
+        if axis_degrees >= 90:
+            neg_cyl_axis_deg = axis_degrees - 90
+        else:
+            neg_cyl_axis_deg = axis_degrees + 90
     else:
-        neg_cyl_axis_deg = axis_degrees + 90
-else:
-    sph_pow_neg_cyl = sphere_power
-    neg_cyl_pow = cylinder_power
-    neg_cyl_axis_deg = axis_degrees
-print("This patient's converted refraction is: " +
-      "S:" + str(sph_pow_neg_cyl) + "  " +
-      "C:" + str(neg_cyl_pow) + "  " +
-      "A:" + str(neg_cyl_axis_deg))
+        sph_pow_neg_cyl = sphere_power
+        neg_cyl_pow = cylinder_power
+        neg_cyl_axis_deg = axis_degrees
+    print("This patient's converted refraction is: " +
+          "S:" + str(sph_pow_neg_cyl) + "  " +
+          "C:" + str(neg_cyl_pow) + "  " +
+          "A:" + str(neg_cyl_axis_deg))
+    return sph_pow_neg_cyl, neg_cyl_pow, neg_cyl_axis_deg
 
-# If cylinder is less than -1.00: Convert refraction to spherical equivalent
-# Conversion is: SE = (S + (C/2))
-if neg_cyl_pow >= -1.00 and neg_cyl_pow <= 0.00:
+def calc_sph_eq():
+    sph_pow_neg_cyl, neg_cyl_pow, neg_cyl_axis_deg = cyl_conv()
+    # Spherical equivalent conversion is: SE = (S + (C/2))
     # Should I pass this into its own function?
     # cyl_pow_conv = cylinder_power
-    spherical_equivalent = (sphere_power + (neg_cyl_pow/2))
-    # BUG: this seems to print regardless of the if statement, not sure why
+    spherical_equivalent = (sph_pow_neg_cyl + (neg_cyl_pow/2))
     print("This patient's spherical equivalent is: " + str(spherical_equivalent))
-else:
-    print("This patient should not have a spherical equivalent.")
+    return spherical_equivalent
 
-# Determine myopic or not
-
-    # If yes: Diagnosis of glaucoma or not?
-
+def myopia_status():
+    spherical_equivalent = calc_sph_eq()
+    # Determine myopic or not
+    if spherical_equivalent <= -6.00:
+        # If yes: Diagnosis of glaucoma or not?
+        glaucoma_dx = str(input("Has this patient been diagnosed with glaucoma (Y/N)? "))
         # If yes: Questions about Cohort 2
+        if glaucoma_dx == "Y":
+            pass
 
         # if no: Questions about Cohort 1
+        else:
+            pass
 
     # If no: Verify axial length:
-
+    else:
+        pt_axial_length = float(input("Please enter the patient's axial length: "))
         # If axial length is greater than 26: Question about glaucoma -> follow the question set above
+        if pt_axial_length >= 26.00:
+            pass
 
         # If axial length is less than 26: Questions about Cohort 3
+        else:
+            pass
 
 
 def main():
-    pass
+    intro()
+    acqr_rfrx()
+    cyl_conv()
+    calc_sph_eq()
+    myopia_status()
 
 if __name__ == '__main__':
     main()
